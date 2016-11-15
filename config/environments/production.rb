8221c121f1b0796b9390e429446921ac24792e79
karankrizz <kirubakarann7@gmail.com>
2016-11-16 01:35:37 +0530

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://sitepoint-actioncable.herokuapp.com',
-                                                 'http://sitepoint-actioncable.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://nauchat.herokuapp.com/']
 
-  config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
+  config.action_cable.url = "wss://nauchat.herokuapp.com/cable"
 end
